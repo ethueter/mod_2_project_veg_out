256ad39f67e73f8bd941f9c31e4b5f320016abd3
github username <github email address>
2019-04-16 10:39:13 -0400

    users stack v5 and sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
         @user=User.new(user_params)
         if @user.valid?
             @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             @error="New user information invalid. Please re-enter."
@@ -30,6 +31,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password_digest, :first_name, :last_name)
+        params.require(:user).permit(:username, :password, :first_name, :last_name, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   has_many  :menu_items
   has_many  :restaurants, through: :reviews
 
+  has_secure_password
+
   validates :username, presence: true, uniqueness: true, length: {minimum: 5}
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password_digest, presence: true
-  validates_confirmation_of :password_digest
+  validates :password, presence: true
+  validates_confirmation_of :password
 
   def full_name
     self.first_name + " " + self.last_name
